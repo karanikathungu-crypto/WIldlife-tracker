4c4f4033e97132c1b7f1b0081a2c08c5ae0c72c4
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-19 13:38:43 +0300

    Add table for sightings

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -3,5 +3,9 @@ SET MODE PostgresSQL;
 CREATE TABLE IF NOT EXISTS animals (
     id int PRIMARY KEY auto_increment,
     name VARCHAR,
-
+)
+CREATE TABLE IF NOT EXISTS sightings (
+    id int,
+    location VARCHAR,
+    rangerName VARCHAR,
 )
