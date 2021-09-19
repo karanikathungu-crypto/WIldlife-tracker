78d588f44fc6d362f80bd34dc76de820848eddae
karanikathungu-crypto <kathungudev@gmail.com>
2021-09-19 18:17:22 +0300

    Add method bodies for methods in the interface the class implements

--- src/main/java/dao/Sql2oAnimalsDao.java
+++ src/main/java/dao/Sql2oAnimalsDao.java
@@ -9,4 +9,33 @@ public class Sql2oAnimalsDao implements AnimalsDao {
     public Sql2oAnimalsDao(Sql2o sql2o){
         this.sql2o = sql2o;
     }
+
+    @Override
+    public void add(Animals animals){
+        String sql = "INSERT INTO animals (id, name) VALUES(:id, :name)";
+        try(Connection conn = sql2o.open()){
+            int id = (int) conn.createQuery(sql, true)
+                    .bind(animals)
+                    .executeUpdate()
+                    .getKey();
+            animals.setId(id);
+        }   catch(Sql2oException e){
+            System.out.println(e);
+        }
+    }
+    @Override
+    public List<Animals>getAll(){
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery("SELECT * FROM animals")
+                    .executeAndFetch(Animals.class);
+        }
+    }
+    @Override
+    public Animals findById(int id){
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery("SELECT * FROM animals WHERE id = :id")
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(Animals.class);
+        }
+    }
 }
